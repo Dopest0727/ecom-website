9c1800ef7472ce4d80e22bb45f56865ef07ed21d
Maurii <maury_0727@hotmail.com>
2025-09-22 19:31:16 +0200

    added stripe and set up API keys

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div></div>;
+import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 5,
+  });
+
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2>Welcome to minimalistic Rusta</h2>
+            <p>Samma produkter, renare hemsida</p>
+            <Button asChild variant="default">
+              Browse all products
+            </Button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
