1777c7618fff4df3ddd8a0adcbe7fd8f4aa20d18
Maurii <maury_0727@hotmail.com>
2025-09-23 18:29:31 +0200

    added cart item to project

--- components/product-detail.tsx
+++ components/product-detail.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import Stripe from "stripe";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { useCartStore } from "@/store/cart-store";
 
 interface Props {
   product: Stripe.Product;
 }
 
 export const ProductDetail = ({ product }: Props) => {
+  const { items, addItem } = useCartStore();
+  const CartItem = items.find((item) => item.id === product.id);
+  const quantity = CartItem ? CartItem.quantity : 0;
   const price = product.default_price as Stripe.Price;
+
+  const onAddItem = () => {
+    addItem({
+      id: product.id,
+      name: product.name,
+      price: price.unit_amount as number,
+      imageUrl: product.images ? product.images[0] : null,
+      quantity: 1,
+    });
+  };
 
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row gap-8 items-center">
@@ -34,8 +50,8 @@ export const ProductDetail = ({ product }: Props) => {
         )}
         <div className="flex items-center space-x-4">
           <Button variant="outline">-</Button>
-          <span className="text-lg font-semibold">27</span>
-          <Button variant="outline">+</Button>
+          <span className="text-lg font-semibold">{quantity}</span>
+          <Button onClick={onAddItem}>+</Button>
         </div>
       </div>
     </div>
